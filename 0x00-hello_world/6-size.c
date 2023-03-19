968c30a0f416f19183a266fd9c07fbf04eb0cf98
jahzeal chimaobi ibeh <jahzealibeh3@gmail.com>
2023-03-19 06:01:08 -0700

    commiting sze of 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,16 @@
 */
 int main(void)
 {
+	char c;
 	int a;
 	float f;
-	long int c;
+	long int u;
 	long long int d;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of  int: %lu byte(s)\n", (unsigned long)sizeof(f));
-printf("Size of  long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of  long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of  float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(u));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
