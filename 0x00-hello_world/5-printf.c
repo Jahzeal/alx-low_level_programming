1ff3c83b259e6d23297f5663aa68411629bf45f5
jahzeal chimaobi ibeh <jahzealibeh3@gmail.com>
2023-03-19 04:52:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>?**
+/**
+  * main - a function that prints a text using
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+			return (0;)
+			}
