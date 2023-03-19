ef24c80bd492d1642390d8ec4b786dd8847389c5
jahzeal chimaobi ibeh <jahzealibeh3@gmail.com>
2023-03-19 08:11:28 -0700

    101-quote.c:10: ERROR

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - Entry point to the function that is printing out a quote
+  *Decription: alx should take it easy
+  *Return: 1
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
