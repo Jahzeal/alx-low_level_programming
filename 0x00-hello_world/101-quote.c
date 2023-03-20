8494320482d080614cff0802444556428ace79d7
jahzeal chimaobi ibeh <jahzealibeh3@gmail.com>
2023-03-20 05:47:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  *main - Entry point to the function that is printing out a quote
-  *Decription: alx should take it easy
-  *Return: 1
-  */
+ *main - Entry point to the function that is printing out a quote
+ *Decription: alx should take it easy
+ *Return: 1
+ */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
-putchar(' ');
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
 }
