f4b19049eed972745e4d4ab0176428357ee18474
jahzeal chimaobi ibeh <jahzealibeh3@gmail.com>
2023-03-20 05:52:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  *main - Entry point to the function that is printing out a quote
  *Decription: alx should take it easy
- *Return: 1
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
